5576ad0c9f4e28a769272c99c71fed553362d3a3
idel <little1student@gmail.com>
2024-07-15 21:59:32 +0300

    init: start HHBot

---
+++ dispatcher/src/main/java/com/bipbup/controllers/TelegramBot.java
@@ -0,0 +1,46 @@
+package com.bipbup.controllers;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+@Log4j
+public class TelegramBot extends TelegramLongPollingBot {
+    @Value("${bot.username}")
+    private String botUsername;
+
+    public TelegramBot(@Value("${bot.token}") String botToken) {
+        super(botToken);
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var originalMessage = update.getMessage();
+        log.debug(originalMessage.getText());
+
+        SendMessage response = new SendMessage();
+        response.setChatId(originalMessage.getChatId());
+        response.setText(originalMessage.getText());
+        sendAnswerMessage(response);
+    }
+
+    private void sendAnswerMessage(SendMessage message) {
+        if (message != null) {
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                log.error(e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+}
