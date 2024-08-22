0a2b811429d787d7951baf5a45dade79844a7345
bulatruslanovich <bulatruslanovich@gmail.com>
2024-08-23 00:07:28 +0300

    feat: add docker-compose.yml & KafkaTopicConfig

--- dispatcher/src/main/java/com/bipbup/config/KafkaTopicConfig.java
+++ dispatcher/src/main/java/com/bipbup/config/KafkaTopicConfig.java
@@ -1,10 +1,15 @@
 package com.bipbup.config;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaTopicConfig {
@@ -20,6 +25,17 @@ public class KafkaTopicConfig {
 
     @Value("${spring.kafka.topics.edit-topic}")
     private String editTopic;
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String server;
+
+    @Bean
+    public KafkaAdmin admin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, server);
+
+        return new KafkaAdmin(configs);
+    }
 
     @Bean
     public NewTopic answerTopic() {
