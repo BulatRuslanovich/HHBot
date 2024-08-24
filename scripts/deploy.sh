9450b84bb47ff61577d0f0ecced672e8c770e61c
bulatruslanovich <bulatruslanovich@gmail.com>
2024-08-24 14:58:12 +0300

    fix: delete origin files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,13 @@ ENV_FILE="./.env"
 
 pushd ~/HHBot/ || exit
 
-docker-compose -f docker-compose.yml --env-file $ENV_FILE down --timeout=60 --remove-orphans
-docker-compose -f docker-compose.yml --env-file $ENV_FILE up --build --detach
+git pull
+
+docker compose -f docker-compose.yml --env-file $ENV_FILE down --timeout=60 --remove-orphans
+docker compose -f docker-compose.yml --env-file $ENV_FILE up --build --detach
 
 popd || exit
+
+docker compose -f docker-compose.yml --env-file .env down --timeout=60 --remove-orphans
+docker compose -f docker-compose.yml --env-file .env up --build --detach
+
