8d3c53dcaab9808ab37235cbdd23cf05f089ce3a
bulatruslanovich <bulatruslanovich@gmail.com>
2024-08-24 15:07:03 +0300

    fix: update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,3 @@ docker compose -f docker-compose.yml --env-file $ENV_FILE down --timeout=60 --re
 docker compose -f docker-compose.yml --env-file $ENV_FILE up --build --detach
 
 popd || exit
-
-docker compose -f docker-compose.yml --env-file .env down --timeout=60 --remove-orphans
-docker compose -f docker-compose.yml --env-file .env up --build --detach
-
