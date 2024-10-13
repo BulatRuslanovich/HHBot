bfd9e189ea2fae59275c7cfa0087aaeba124a79d
bulatruslanovich <bulatruslanovich@gmail.com>
2024-10-13 14:54:04 +0300

    feat: test commit

--- node/src/main/java/com/bipbup/NodeApplication.java
+++ node/src/main/java/com/bipbup/NodeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+
 @EnableCaching
 @EnableScheduling
 @EnableJpaRepositories("com.bipbup")
